5686d118bd03f58611c6037dfd65a6c055bdeb27
Gokul Nath KP <gokulnath.kpgn@gmail.com>
2017-10-28 16:13:44 +0530

    Integrate Picasso.

--- app/src/main/java/com/kpgn/networkingdemo/activity/WeatherActivity.java
+++ app/src/main/java/com/kpgn/networkingdemo/activity/WeatherActivity.java
@@ -3,6 +3,7 @@ package com.kpgn.networkingdemo.activity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.kpgn.networkingdemo.R;
@@ -13,6 +14,7 @@ import com.kpgn.networkingdemo.event.WeatherDataFailureEvent;
 import com.kpgn.networkingdemo.event.WeatherDataSuccessEvent;
 import com.kpgn.networkingdemo.manager.ConnectionManager;
 import com.squareup.otto.Subscribe;
+import com.squareup.picasso.Picasso;
 import com.wang.avi.AVLoadingIndicatorView;
 
 import butterknife.BindView;
@@ -28,6 +30,9 @@ public class WeatherActivity extends BaseActivity {
 
     @BindView(R.id.weather_data_container)
     View mWeatherDataContainer;
+
+    @BindView(R.id.iv_condition)
+    ImageView mCondition;
 
     @BindView(R.id.tv_cloudy)
     TextView mCloudy;
@@ -104,8 +109,9 @@ public class WeatherActivity extends BaseActivity {
             mWeatherDataContainer.setVisibility(View.VISIBLE);
             mErrorContainer.setVisibility(View.GONE);
             mCloudy.setText(successEvent.weatherData.current.condition.text);
-            mTempInC.setText(getString(R.string.temperature_string, successEvent.weatherData.current.tempInF));
-            mTempInF.setText(getString(R.string.temperature_string, successEvent.weatherData.current.tempInC));
+            mTempInC.setText(getString(R.string.temperature_string_f, successEvent.weatherData.current.tempInF));
+            mTempInF.setText(getString(R.string.temperature_string_c, successEvent.weatherData.current.tempInC));
+            Picasso.with(this).load("http:" + successEvent.weatherData.current.condition.icon).into(mCondition);
         } else {
             mWeatherDataContainer.setVisibility(View.GONE);
             mErrorContainer.setVisibility(View.VISIBLE);
